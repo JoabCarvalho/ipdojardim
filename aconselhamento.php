609eb54905d110472186276f9f95a7f77b2bdcc6
Joab Carvalho <carvalho.morais.sales@gmail.com>
2023-09-14 17:35:05 -0300

    adicionar os arquivos

---
+++ aconselhamento.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>IP do Jardim</title>
+    <link rel="shortcut icon" href="Imagens/Logo/Ico/Logo.ico" type="image/x-icon">
+    <link rel="stylesheet" href="Style/Style.css">
+</head>
+<body>
+    <header>
+        <div class="menu">
+            <p>Igreja Presbiteriana do Jardim</p>
+        </div>
+    </header>
+    <nav>
+        <div class="menu">
+            <a href="index.php">Início</a>
+            <a href="noQueCremos.php">No que cremos</a>
+            <a href="aconselhamento.php">Aconselhamento</a>
+            <a href="redeSociais.php">Redes Sociais</a>
+        </div>
+        <div>                  
+            <img src="Imagens/Logo/Ico/Logo192x192.png" alt="Logo IP do Jardim">
+            <h1>Igreja Presbiteriana do Jardim</h1>
+            <h2>Av. Ricardo Loureiro Cavalcante, nº 133, Jardim Aeroporto, Bayeux</h2>
+        </div>
+    </nav>
+    <main> 
+    <h2>Aconselhamento Pastoral</h2>
+    <form action="aconselhamento.php" method="POST">
+        <fieldset>
+            <legend>Entre em contato o nosso Pastor</legend>
+                <br><br>
+                
+            <input type="submit" name="submit" id="submit">
+        </fieldset>
+        </form>
+    </main>
+    <footer>
+        <p> Desenvolvido pela Igreja Presbiteriana do Jardim</p>
+    </footer>
+</body>
+</html>
---
+++ redeSociais.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>IP do Jardim</title>
+    <link rel="shortcut icon" href="Imagens/Logo/Ico/Logo.ico" type="image/x-icon">
+    <link rel="stylesheet" href="Style/Style.css">
+</head>
+<body>
+    <header>
+        <div class="menu">
+            <p>Igreja Presbiteriana do Jardim</p>
+        </div>
+    </header>
+    <nav>
+        <div class="menu">
+            <a href="index.php">Início</a>
+            <a href="noQueCremos.php">No que cremos</a>
+            <a href="aconselhamento.php">Aconselhamento</a>
+            <a href="redeSociais.php">Redes Sociais</a>
+        </div>
+        <div>                  
+            <img src="Imagens/Logo/Ico/Logo192x192.png" alt="Logo IP do Jardim">
+            <h1>Igreja Presbiteriana do Jardim</h1>
+            <h2>Av. Ricardo Loureiro Cavalcante, nº 133, Jardim Aeroporto, Bayeux</h2>
+        </div>
+    </nav>
+    <main> 
+        <h2>Conheça-nos nas Redes Sociais</h2>
+        <div class="redeSociais">
+        <ul>
+            <li><a href="https://www.instagram.com/ipdojardim/" target="_blank">Instagram</a></li>
+            <li><a href="https://www.youtube.com/@ipdojardim" target="_blank">YouTube</a></li>
+            <li><a href="https://drive.google.com/drive/folders/1GcUW57k94WoUZ29WeGNGF2a259Zd2oDD" target="_blank">Devocional entre Famílias</a></li>
+        </ul>
+        </div>
+    </main>
+    <footer>
+        <p> Desenvolvido pela Igreja Presbiteriana do Jardim</p>
+    </footer>
+</body>
+</html>
