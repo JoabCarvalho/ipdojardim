8fcc5a350fcbb61867c97d8a40fc121a6c40a0f3
Joab Carvalho <carvalho.morais.sales@gmail.com>
2023-09-22 19:13:58 -0300

    Atualizando

--- aniversariantes.php
+++ aniversariantes.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <?php
     include_once('config.php');
     $sql = "SELECT DATE_FORMAT(m.data_nascimento, '%d/%m/%Y') AS data_formatada, m.conhecido FROM membros m WHERE MONTH(m.data_nascimento) = MONTH(NOW()) ORDER BY data_formatada";
@@ -55,59 +54,4 @@
         <p> Desenvolvido pela Igreja Presbiteriana do Jardim</p>
     </footer>
 </body>
-=======
-<?php
-    include_once('config.php');
-    $sql = "SELECT DATE_FORMAT(m.data_nascimento, '%d/%m/%Y') AS data_formatada, m.conhecido FROM membros m WHERE MONTH(m.data_nascimento) = MONTH(NOW()) ORDER BY data_formatada";
-    $result = $conexao->query($sql);
-    
- ?>
-<!DOCTYPE html>
-<html lang="pt-br">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>IP do Jardim</title>
-    <link rel="shortcut icon" href="Imagens/Logo/Ico/Logo.ico" type="image/x-icon">
-    <link rel="stylesheet" href="Style/Style.css">
-    
-</head>
-<body>
-    <header>
-        <div class="menu">
-            <p>Igreja Presbiteriana do Jardim</p>
-        </div>
-    </header>
-    <nav>
-        <div class="menu">
-            <a href="index.php">Início</a>
-            <a href="noQueCremos.php">No que cremos</a>
-            <a href="aconselhamento.php">Aconselhamento</a>
-            <a href="video.php">Pregação</a>
-            <a href="aniversariantes.php">Aniversariantes</a>
-            <a href="redeSociais.php">Redes Sociais e Devocionais</a>
-        <div>                  
-            <img src="Imagens/Logo/Ico/Logo192x192.png" alt="Logo IP do Jardim">
-            <h1>Igreja Presbiteriana do Jardim</h1>
-            <h2>Av. Ricardo Loureiro Cavalcante, nº 133, Jardim Aeroporto, Bayeux</h2>
-        </div>
-    </nav>
-    <main> 
-        <h2>Aniversariantes do Mês</h2>
-        <br><br><br><br>
-        <div class="aniversariantes">
-            <?php
-            
-                while($user_data = mysqli_fetch_assoc($result)) {
-                    echo "<li> " .$user_data['data_formatada']. " - " .$user_data['conhecido']. "</li>";
-                }
-            
-            ?>
-        </div>
-    </main>
-    <footer>
-        <p> Desenvolvido pela Igreja Presbiteriana do Jardim</p>
-    </footer>
-</body>
->>>>>>> 1eea8ad27a3368c2bf194d90c838d4012847ee6d
 </html>
--- noQueCremos.php
+++ noQueCremos.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <!DOCTYPE html>
 <html lang="pt-br">
 <head>
@@ -59,66 +58,4 @@
         <p> Desenvolvido pela Igreja Presbiteriana do Jardim</p>
     </footer>
 </body>
-=======
-<!DOCTYPE html>
-<html lang="pt-br">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>IP do Jardim</title>
-    <link rel="shortcut icon" href="Imagens/Logo/Ico/Logo.ico" type="image/x-icon">
-    <link rel="stylesheet" href="Style/Style.css">
-    
-</head>
-<body>
-    <header>
-        <div class="menu">
-            <p>Igreja Presbiteriana do Jardim</p>
-        </div>
-    </header>
-    <nav>
-        <div class="menu">
-            <a href="index.php">Início</a>
-            <a href="noQueCremos.php">No que cremos</a>
-            <a href="aconselhamento.php">Aconselhamento</a>
-            <a href="video.php">Pregação</a>
-            <a href="aniversariantes.php">Aniversariantes</a>
-            <a href="redeSociais.php">Redes Sociais e Devocionais</a>
-        <div>
-        <div>                  
-            <img src="Imagens/Logo/Ico/Logo192x192.png" alt="Logo IP do Jardim">
-            <h1>Igreja Presbiteriana do Jardim</h1>
-            <h2>Av. Ricardo Loureiro Cavalcante, nº 133, Jardim Aeroporto, Bayeux</h2>
-        </div>
-    </nav>
-    <main> 
-        <h2>O Credo Apostólico</h2>
-        <h3>ORIGEM</h3>
-            <p>Credo Apostólico, o mais conhecido dos credos, é atribuído pela tradição aos doze apóstolos. Mas os estudiosos acreditam que ele se desenvolveu a partir de pequenas confissões batismais empregadas nas igrejas dos primeiros séculos. Embora os seus artigos sejam de origem bem antiga, acredita-se atualmente que o credo apostólico só alcançou sua forma definitiva por volta do sexto século, quando são encontrados registros do seu emprego na liturgia oficial da igreja ocidental. De um modo ou de outro, parece evidente sua conexão com outros credos antigos menores; como os seguintes:</p>
-            <p>Creio em Deus Pai Todo-poderoso, e em Jesus Cristo, seu único Filho, nosso Senhor. E no Espírito Santo, na santa Igreja, na ressurreição da carne.</p>
-            <p>Creio em Deus Pai Todo-poderoso. E em Jesus Cristo seu único Filho nosso Senhor, que nasceu do Espírito Santo e da virgem Maria; concebido sob o poder de Pôncio Pilatos e sepultado; ressuscitou ao terceiro dia; subiu ao céu e está sentado à mão direita do Pai, de onde há de vir julgar os vivos e os mortos. E no Espírito Santo; na santa Igreja; na remissão dos pecados; na ressurreição do corpo.</p>
-            <p>O Credo Apostólico, assim como os Dez Mandamentos e a Oração Dominical, foi anexado, pela Assembléia de Westminster, ao Catecismo. “Não como se houvesse sido composto pelos apóstolos, ou porque deva ser considerado Escritura canônica, mas por ser um breve resumo da fé cristã, por estar de acordo com a palavra de Deus, e por ser aceito desde a antigüidade pelas igrejas de Cristo.”</p>
-
-        <h3>TEXTO EM PORTUGUÊS</h3>
-            <p>Creio em Deus Pai, Todo-poderoso, Criador do Céu e da terra.</p>
-            <p>Creio em Jesus Cristo, seu único Filho, nosso Senhor, o qual foi concebido por obra do Espírito Santo; nasceu da virgem Maria; padeceu sob o poder de Pôncio Pilatos, foi crucificado, morto e sepultado; ressurgiu dos mortos ao terceiro dia; subiu ao Céu; está sentado à direita de Deus Pai Todo-poderoso, donde há de vir para julgar os vivos e os mortos.</p>
-            <p>Creio no Espírito Santo; na Santa Igreja Universal; na comunhão dos santos; na remissão dos pecados; na ressurreição do corpo; na vida eterna. Amém.</p>
-        
-        <?php
-        /*echo 
-        "<h3>NOTAS</h3>
-        <p>* Extraído de Paulo Anglada, Sola Scriptura: A Doutrina Reformada das Escrituras (São Paulo: Os Puritanos, 1998), 178-79.</p>
-        <p>[1] Alguns chegaram a sugerir que cada apóstolo teria contribuído com um artigo.</p>
-        <p>[2] Schaff, Creeds of Christendom, vol.1, 20. Citado por A. A. Hodge, Outlines of Theology (Edinburgh, & Pennsylvania: The Banner of Truth Trust, 1991), 115.</p>
-        <p>[3] O primeiro desses credos provém, provavelmente, da primeira metade do segundo século. O segundo, conhecido como Credo Romano Antigo, provém da segunda metade do segundo século. Ver O. G. Oliver Jr., “Credo dos Apóstolos,” em Enciclopédia Histórico-Teológica da Igreja Cristã, vol. 1 (São Paulo: Vida Nova, 1993): 362-63.</p>
-        <p>[4] Citado por A. A. Hodge, Outlines of Theology, 115.</p>
-        <p>[5] Do Psalterium Aethelstani. Citado por Frans Leonard Schalkwijk, Coinê: Pequena Gramática do Grego Neotestamentário, 5 ed. (Patrocínio-MG: CEIBEL, 1989), 109.</p>"
-        */?>
-        
-    </main>
-    <footer>
-        <p> Desenvolvido pela Igreja Presbiteriana do Jardim</p>
-    </footer>
-</body>
->>>>>>> 1eea8ad27a3368c2bf194d90c838d4012847ee6d
 </html>
